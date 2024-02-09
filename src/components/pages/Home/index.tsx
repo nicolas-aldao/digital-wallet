c7a60725101ba7296391e52cec4c3a385d2376bd
Nicolás Aldao <nicolasaldao@live.com>
2024-02-09 19:08:16 -0300

    set up

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,13 +1,16 @@
-import { useSelector, useDispatch } from "react-redux";
+import {
+  //useSelector,
+  useDispatch,
+} from "react-redux";
 import { saveUserData } from "../../../features/userSlice";
-import { increment } from "../../../features/counterSlice";
+// import { increment } from "../../../features/counterSlice";
 import { CardBalance } from "../../organisms/CardBalance/index";
 import { useGetUserById } from "../../../hooks/useGetUserById";
 import { UserFullName } from "../../organisms/UserFullName/index";
 
 export const Home = () => {
   const [data] = useGetUserById("65b32b744548d2c06ed245b2");
-  const count = useSelector((state: any) => state.counter.value);
+  // const count = useSelector((state: any) => state.counter.value);
   const dispatch = useDispatch();
   dispatch(saveUserData(data));
 
